58d54751887886cd69c4bc3d98f16c882d3b1c33
markstewart <markstewy@gmail.com>
2017-02-27 14:36:54 -0700

    adding grayscale.js to navbar directive link

--- public/app/nav/navDir.js
+++ public/app/nav/navDir.js
@@ -1,9 +1,40 @@
 angular.module('app.nav')
 	.directive('navDir', function() {
   return {
-    restrict: 'EA',
+    restrict: 'E',
     templateUrl: './app/nav/nav.html',
     controller: 'navCtrl',
-	controllerAs: 'navCtrl'
+	controllerAs: 'navCtrl',
+	link: function() {
+		// jQuery to collapse the navbar on scroll
+		function collapseNavbar() {
+			console.log($('.navbar'));
+			console.log($(".navbar").offset());
+		    if ($(".navbar").offset().top > 50) {
+		        $(".navbar-fixed-top").addClass("top-nav-collapse");
+		    } else {
+		        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+		    }
+		}
+
+		$(window).scroll(collapseNavbar);
+		collapseNavbar();
+
+		// jQuery for page scrolling feature - requires jQuery Easing plugin
+		$(function() {
+		    $('a.page-scroll').bind('click', function(event) {
+		        var $anchor = $(this);
+		        $('html, body').stop().animate({
+		            scrollTop: $($anchor.attr('href')).offset().top
+		        }, 1500, 'easeInOutExpo');
+		        event.preventDefault();
+		    });
+		});
+
+		// Closes the Responsive Menu on Menu Item Click
+		$('.navbar-collapse ul li a').click(function() {
+		    $(this).closest('.collapse').collapse('toggle');
+		});
+	}
   };
 });
